6841b9dabbc87fa679f841efff21841c1aeba2cb
Shantnu Garg <gargshantnu@gmail.com>
2022-06-14 17:30:41 +0530

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,7 +1,6 @@
-var APP_ID = "3fc37404bd137c29be0f5337bad8211f2";
+var APP_ID = "357f8b08576273bf8881133155bfc5568";
 
-var userId = 'debug4'; //Todo: replace with your userId and password
+var userId = 'debug4'; //Todo: replace with your agent id and password
 var password = 'debug4';
-// var botId = 'abhishek-zbkki';
-// var agentId = 'abhishek@kommunicate.io';
-// var APP_ID = "357f8b08576273bf8881133155bfc5568";
+// var botId = 'df-bot-sbefo';
+// var agentId = 'shantnu+18apr2021@kommunicate.io';
