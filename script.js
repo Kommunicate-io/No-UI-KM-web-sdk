56331a27a9583120a23ed38262d94e5613c1129d
Shantnu Garg <gargshantnu@gmail.com>
2022-06-14 11:20:14 +0530

    add getGroupList function

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-function getGroupList
+const baseUrl = 'https://chat.kommunicate.io'
+
+function getGroupList() {
+    Applozic.ALApiService.loadGroups({
+        data: {
+            baseUrl: baseUrl,
+        },
+        success: function (response) {
+            // most recent group will come at last in response
+            console.log(response.response);
+        },
+    })
+}
