1bd77643701c39c158e9f088b4743b476f9043b0
Shantnu Garg <gargshantnu@gmail.com>
2022-06-14 11:13:00 +0530

    move constants to seperate file

---
+++ constants.js
@@ -0,0 +1,6 @@
+var APP_ID = "357f8b08576273bf8881133155bfc5568";
+
+var userId = 'debug4'; //Todo: replace with your userId and password
+var password = 'debug4';
+// var botId = 'abhishek-zbkki';
+// var agentId = 'shantnu+18apr2021@kommunicate.io';
