87f794d18968b3ab14bffdc0935b0fb628d8c721
Abhishek Singh <aks3555@gmail.com>
2022-06-17 11:19:47 +0530

    updated changes

--- constants.js
+++ constants.js
@@ -1,7 +1,8 @@
-var APP_ID = "357f8b08576273bf8881133155bfc5568";
+var APP_ID = "3fc37404bd137c29be0f5337bad8211f2";
 
-var userId = 'debug4'; //Todo: replace with your agent id and password
-var password = 'debug4';
+var userId = 'abhishek@kommunicate.io'; //Todo: replace with your agent id and password
+var password = 'Abhi8560';
 var socketConnected = false;
+var KM_BASE_URL = "https://chat.kommunicate.io"
 // var botId = 'df-bot-sbefo';
 // var agentId = 'shantnu+18apr2021@kommunicate.io';
--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-const baseUrl = 'https://chat.kommunicate.io'
-
+function init_KM(appId, response, events){
+    window.Applozic.ALSocket.init(appId, response, events);
+}
 function getGroupList() {
     Applozic.ALApiService.loadGroups({
         data: {
-            baseUrl: baseUrl,
+            baseUrl: KM_BASE_URL,
         },
         success: function (response) {
             // most recent group will come at last in response
@@ -34,6 +35,25 @@ function sendAttachment(groupId, file) {
 }
 
 
+// The file name of encrypted media from user's end will have "AWS-ENCRYPTED-" prepended to it name. 
+// You can use this to identify if a media is encrypted or not. 
+// for encrypted media, pass the blobkey in the function below. This returns the url of the media file.
+function getURLFromBlobKey(blobKey, callback) {
+    var params = '?key=' + blobKey;
+    window.Applozic.ALApiService.ajax({
+        type: 'GET',
+        global: false,
+        url: KM_BASE_URL + '/rest/ws/file/url' + params,
+        success: function (res) {
+            callback(null, res);
+        },
+        error: function (err) {
+            callback(err);
+        },
+        skipEncryption: true
+    })
+};
+
 
 function sendDeliveryReport(messageKey) {
     Applozic.ALApiService.sendDeliveryUpdate({
@@ -54,6 +74,9 @@ function sendReadReport(groupId) {
     });
 }
 
+function sendTypingStatus(typingStatus1, typingStatus2, userId, groupId){
+    window.Applozic.ALSocket.sendTypingStatus(typingStatus1, typingStatus2, userId, groupId);
+}
 
 function getLatestMessageList() {
     Applozic.ALApiService.getMessages({
@@ -115,4 +138,71 @@ function sendMessage(groupId, message){
         success: function (response) { console.log(response); },
         error: function () { }
     });
+}
+
+
+function onConnectFailed(resp) {
+    console.log("onConnectFailed triggered");
+    if (navigator.onLine) {
+        console.log("reconnecting");
+        window.Applozic.ALSocket.reconnect();
+    }
+}
+function subscribeToTypingChannel(groupId){
+    window.Applozic.ALSocket.subscibeToTypingChannel(groupId);
+}
+
+function unsubscribeToTypingChannel(){
+    window.Applozic.ALSocket.unsubscibeToTypingChannel();
+}
+
+function subscribeToSupportChannel(){
+    window.Applozic.ALSocket.subscribeToSupportChannel();
+}
+function unsubscribeToSupportChannel(){
+    window.Applozic.ALSocket.unsubscribeToSupportChannel();
+}
+// anybody, (agent/user) when goes online, offline, or away, this will get triggered.
+//  KM_USER_STATUS: {
+    //     OFFLINE: 0,
+    //     ONLINE: 1,
+    //     AWAY: 2,
+    //     NOT_AWAY: 3,
+    // 
+// },
+function userStatusUpdateHandler(resp){
+    console.log('onUserStatusUpdate', JSON.parse(resp.body)["message"]);
+}
+
+function updateAgentStatus(statusId){
+    Applozic.ALSocket.sendStatus(statusId)
+}
+
+// you can identify the messages with help of message keys
+function onMessageRead (resp) {
+    // will trigger when the end user reads the message sent by you
+    console.log('message read', resp)
+}
+function onMessageDelivered (resp) {
+    // will trigger when the sent message gets delivered
+    console.log('message read', resp)
+}
+
+
+// added event handlers for events which are not configured
+function onMessageHandler(message){
+    let messageType = message.type;
+    console.log(message);
+    let MESSAGE_TYPE_ACTION_MAP = {
+        "APPLOZIC_25": function(message){
+            // user online offline status changed
+            console.log('here');
+            console.log(message);
+        },
+    }
+
+    let action  = MESSAGE_TYPE_ACTION_MAP[messageType];
+    if(!action) return;
+    console.log(messageType);
+    action(message);
 }
